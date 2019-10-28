e9c02bc3152c3ca6b94d1343bb70f0908a2a8b18
hoiyoo <hoi_yoo@yahoo.com>
2019-10-27 22:23:08 -0400

    Update check.java

--- check.java
+++ check.java
@@ -18,3 +18,69 @@ public class check {
 
 }
 }
+
+
+
+public class  LongestCommonSubString
+
+{
+
+    /** function lcs **/
+
+    public String lcs(String str1, String str2)
+
+    {
+    	
+        int l1 = str1.length();
+        int l2 = str2.length();
+
+        char[] aa = str1.toCharArray();
+        
+        int[][] arr = new int[l1 + 1][l2 + 1];
+        int len = 0, pos = -1;
+
+        for (int x = 1; x < l1 + 1; x++)
+        {
+            for (int y = 1; y < l2 + 1; y++)
+            {
+                if (aa[x - 1] == str2.charAt(y - 1))
+                {
+                        arr[x][y] = arr[x - 1][y - 1] + 1;
+                        if (arr[x][y] > len)
+                        {
+                            len = arr[x][y];
+                            pos = x;
+                        }               
+                }
+                else
+                    arr[x][y] = 0;
+            }
+        }        
+
+        return str1.substring(pos - len, pos);
+
+    }
+
+ 
+
+    /** Main Function **/
+
+    public static void main(String[] args) throws IOException
+
+    {    
+
+   
+        String str1="aabcdefg12ttxx";
+        String str2="ddbcdefg15ttxx";
+
+        LongestCommonSubString obj = new LongestCommonSubString(); 
+
+        String result = obj.lcs(str1, str2);
+
+ 
+
+        System.out.println("\nLongest Common Substring : "+ result);
+
+    }
+
+}
