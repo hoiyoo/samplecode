72c21d6a332e1c3764f07d048cf29483877839f5
hoiyoo <hoi_yoo@yahoo.com>
2019-11-05 21:51:47 -0500

    Update check.java

--- check.java
+++ check.java
@@ -1,3 +1,27 @@
+public void json2Object() {
+		// given
+		String jsonString = "{\"name\":\"Soo Philip Kim\",\"emailAddress\":\"philipjkim@gmail.com\",\"department\":\"DEVELOPMENT\",\"yearJoined\":2012}";
+
+		// when
+		JSONObject object = (JSONObject) JSONValue.parse(jsonString);
+
+		// then
+		@SuppressWarnings("unchecked")
+		Set<String> keySet = object.keySet();
+		for (String key : keySet) {
+			Object value = object.get(key);
+			System.out.printf("%s=%s (%s)\n", key, value, value.getClass()
+					.getSimpleName());
+		}
+
+		assertThat(object.get("name").toString(), is("Soo Philip Kim"));
+		assertThat(object.get("emailAddress").toString(),
+				is("philipjkim@gmail.com"));
+		assertThat(object.get("department").toString(), is("DEVELOPMENT"));
+		assertThat(object.get("yearJoined").toString(), is("2012"));
+	}
+
+
 package name.fraser.neil.plaintext;
 
 public class check {
